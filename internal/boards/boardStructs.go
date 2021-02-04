23e094c6ea60be0e8a925ad1f22b12b44e17afd6
garydunkerley <garydunkerley@brandeis.edu>
2021-02-03 21:19:32 -0500

    blagh

---
+++ internal/boards/boardStructs.go
@@ -0,0 +1,30 @@
+package boards
+
+type node struct {
+	name      string
+	id        int
+	neighbors []*node
+	color     int8        // color is 0 for empty, 1 for black, 2 for white
+	inGroup   *stoneGroup // is a part of a given stone group
+}
+
+type stoneGroup struct {
+	//	id      int        // in case we want to refer to them?
+	stones    map[*node]bool // what stones are in here? Need to make all empty if group dies
+	liberties map[*node]bool // what liberties does the group have
+	// https://softwareengineering.stackexchange.com/questions/177428/sets-data-structure-in-golang
+}
+
+type goGraph struct {
+	nodes        map[int]*node
+	coords       map[[2]int]int
+	coord_bounds []int //for example, [9,9] for a 9 by 9 board
+}
+
+type boardState struct {
+	current_board goGraph
+	white_to_move bool
+	move_history  []string
+	board_history []goGraph // to determine if move violates ko rule
+	move_count    int
+}
--- internal/gameplay/getNeighbors.go
+++ internal/gameplay/getNeighbors.go
@@ -2,39 +2,42 @@ package gameplay
 
 // this script combines the get_same_color and get_opp_color functions from the original script.
 
-func getNeighbors(x *node, neighb_color int) {
+func get_same_color_neighbors(x *node) []*node {
 	var friends []*node
-	var enemies []*node
-	var liberties []*node
-
-	if neighb_color == x.color {
-		for _, y := range x.neighbors {
-			if y.color == x.color {
-				friends = append(friends, y)
-			}
-		}
-
+	if x.color == 0 {
 		return friends
+	}
+	for _, y := range x.neighbors {
+		if y.color == x.color {
+			friends = append(friends, y)
+		}
+	}
+	return friends
+}
 
+func get_opp_color_neighbors(x *node) []*node {
+	var enemies []*node
+	if x.color == 0 {
+		return enemies
+	}
+	for _, y := range x.neighbors {
+		if y.color != x.color && y.color != 0 {
+			enemies = append(enemies, y)
+		}
+	}
+	return enemies
+}
+
+func get_liberties(x *node) []*node {
+	var liberties []*node
+	if x.color == 0 {
+		return liberties
 	} else {
-
-		if neighb_color != 0 {
-
-			for _, y := range x.neighbors {
-				if y.color != x.color && y.color != 0 {
-					enemies = append(enemies, y)
-				}
-			}
-
-			return enemies
-
-		} else {
-			for _, y := range x.neighbors {
-				if y.color == 0 {
-					liberties = append(liberties, y)
-				}
+		for _, y := range x.neighbors {
+			if y.color == 0 {
+				liberties = append(liberties, y)
 			}
 		}
-
 	}
+	return liberties
 }
