92f9dbe2dc5d25469111a11613caa47aed24703d
garydunkerley <garydunkerley@brandeis.edu>
2021-02-08 02:16:35 -0500

    boardTop_to_goGraph.go

--- internal/game/boardTop_to_goGraph.go
+++ internal/game/boardTop_to_goGraph.go
@@ -1,28 +1,42 @@
 package game
 
 import (
-
 	"strconv"
-
 )
-(a boardTop) func ConvertToGoGraph() goGraph {
+
+func (a boardTop) ConvertToGoGraph() goGraph {
 	var ourBoard goGraph
 	var newNode *node
 	var nodeName string
 
-	nodes := make(map[int]*node)
+	ourNodes := make(map[int]*node)
 
-	for i:= 0; i < a.node_count; i++ {
-		if boardTop.coords == nil {
-			nodeName = strconv.Itoa(i) 
-		} else {
-			for j:= 0; j < a.node_count; j++ {
-			nodeName = strconv()+ "-" +
+	if a.coords == nil {
+		// if the coordinate map is blank, initialize nodes
+		// whose node name is the conversion of the node id into a string
+		for i := 0; i < a.node_count; i++ {
+			newNode = initializeNode(i, strconv.Itoa(i))
+			ourNodes[i] = newNode
 		}
-		newNode = initializeNode(i, nodeName)
-		nodes[i]=newNode
+	} else {
+		// if the coordinate map is not blank, initialize
+		// nodes whose names obey a grid convention
+		for key := range boardTop.coords {
+			nodeName = strconv.Itoa(key[0]) + "-" + strconv.Itoa(key[1])
+			newNode = initializeNode(a.coords[key], nodeName)
+			ourNodes[a.coords[key]] = newNode
+		}
 
 	}
+	for i := 0; i < a.node_count; i++ {
+		for z := range a.edges[i] {
+			ourNodes[i].neighbors = append(ourNodes[i].neighbors, ourNodes[z])
+		}
+	}
+
+	ourBoard.nodes = ourNodes
+	ourBoard.boardTop = a
+
+	return ourBoard
 
 }
-
