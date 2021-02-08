208720b429a8ec85b1419ffb768d97910f7e9732
Justin Rogers <justin_rogers@math.ucsb.edu>
2021-02-08 14:11:28 -0800

    Fix documentation

--- internal/game/getStringsToMergeOrCapture.go
+++ internal/game/getStringsToMergeOrCapture.go
@@ -5,27 +5,25 @@ func getOppColor(color int8) int {
 	return 3 - color
 }
 
-// getSubsumedStrings gives us a map encoding the stoneStrings that need to be subsumed into a larger stoneString given a play at nodeId of a given color
+// getSubsumedStrings gives us a map encoding the stoneStrings that need
+// to be subsumed into a larger stoneString given a play at nodeId of a given color
 
 func (G GoGraph) getSubsumedStrings(nodeId int, color int8) map[stoneString]bool {
-
 	stringsToBeMerged := make(map[stoneString]bool)
 	var friendlies []int
-
 	for z := range G.nodes[nodeId].neighbors {
 		if z.color == color {
 			friendlies = append(friendlies, z.id)
 		}
 	}
-
 	for z := range friendlies {
 		stringsToBeMerged[G.stringOf(z)] = true
 	}
-
 	return stringsToBeMerged
 }
 
-// getCapturedStrings gives a map encoding the stoneStrings that will be captured by a play at NodeId of a given color.
+// getCapturedStrings gives a map encoding the stoneStrings
+// that will be captured by a play at NodeId of a given color.
 func (G GoGraph) getCapturedStrings(nodeId int, color int8) map[stoneString]bool {
 	capturedStrings := make(map[stoneString]bool)
 	var potentialCaptives []int
