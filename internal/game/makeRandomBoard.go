96379852af63dc627fd219be26d217a46051bb34
Gary Dunkerley <58280110+garydunkerley@users.noreply.github.com>
2021-02-08 01:19:57 -0500

    Create makeRandomBoard.go

---
+++ internal/game/makeRandomBoard.go
@@ -0,0 +1,40 @@
+package game
+
+import (
+	"math/rand"
+	"strconv"
+	"time"
+)
+
+func makeRandomBoard(n int, prob float64) GoGraph {
+	// makeRandomBoard creates a board topology on n nodes such that any two nodes are connected via an edge with a fixed probability.
+	var ourTopology boardTop
+
+	edges := make(map[int][]int)
+
+	ourTopology.node_count = n
+
+	for i = 0; i < n; i++ {
+		if i < n-1 {
+			edges[i] = append(edges[i], i+1)
+
+			for j := i + 2; j < n; j++ {
+				rand.Seed(time.Now().UnixNano())
+				v = rand.Float64
+
+				if v < prob {
+					edges[i] = append(edges[i], j)
+					edges[j] = append(edges[j], i)
+				}
+			}
+		}
+		if i > 0 {
+			edges[i] = append(edges[i], i-1)
+		}
+
+	}
+
+	ourTopology.edges = edges
+
+	return ourTopology
+}
