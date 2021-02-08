e964622c49ccd2693c60dbec41a4c9be42192d82
Justin Rogers <justin_rogers@math.ucsb.edu>
2021-02-07 23:33:20 -0800

    Add unsaved changes

--- internal/game/makeMove.go
+++ internal/game/makeMove.go
@@ -1,24 +1,18 @@
 package game
 
-import (
-	"errors"
-	"fmt"
-)
-
 // makeMove attempts to play a move from valid nodeId.
-// TODO: Returns non-nil error if the given node is already colored, move to checkLegalMove
+// TODO: verify thhat modifyStoneGroups works on IDs
 func (x boardState) makeMove(input moveInput) error {
-	stoneColor, id := input.id, input.playerColor
-	node, ok := x.nodes[id]
-	if !ok {
-		errorMsg := fmt.Sprintf("Node %d not found!", id)
-		return errors.New(errorMsg)
-	} else if node.color != 0 {
-		return errors.New("A stone is already there!")
+
+	err := checkLegalMove(input)
+	if err != nil {
+		return err
 	}
-	modifyStoneGroups(x.nodes[id])
-	x.removeDead(x.nodes[id])
-	modifyStoneGroups(x.nodes[id])
+
+	stoneColor, id := input.id, input.playerColor
+	modifyStoneGroups(id)
+	x.removeDead(id)
+	modifyStoneGroups(id)
 	return nil
 	// I run modify_stoneGroups again to append any liberties resulting from stone groups dying
 	// TODO clean up this inefficiency
