0fbe1a4878ffd6df057c347e233f0d5a3729b875
Justin Rogers <justin_rogers@math.ucsb.edu>
2021-02-10 19:41:55 -0800

    rename countLiberties.go

---
+++ internal/game/countLiberties.go
@@ -0,0 +1,12 @@
+package game
+
+func (myGraph GoGraph) countLiberties(S stoneString) int {
+	liberties := make(map[int]bool)
+	for x := range S.stones {
+		for y := range getLiberties(myGraph.nodes[x]) {
+			liberties[y] = true
+		}
+
+	}
+	return len(liberties)
+}
