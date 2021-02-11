da03e0aaf5a6dd33e0bee38740727f3f1b24dcf5
Justin Rogers <justin_rogers@math.ucsb.edu>
2021-02-11 10:32:05 -0800

    Add tests

--- internal/game/gameRunner_test.go
+++ internal/game/gameRunner_test.go
@@ -20,3 +20,58 @@ func TestMove(t *testing.T) {
 		t.Fatal(state) // log end state
 	}
 }
+
+// Test liberties for an edge move
+func TestEdgeLiberties(t *testing.T) {
+	state := initBoardState(makeSquareBoard(9, 9), 6)
+	ID := 1
+	err := state.moveByID(ID)
+	if err != nil { // log any error from this move
+		t.Logf("Move %v: %v\n Error: %v", 0, ID, err)
+	}
+
+	libCount := state.countLiberties(state.stringOf[ID])
+	if libCount != 3 {
+		t.Logf("Edge test: expected 3 liberties, have %v", libCount)
+		t.Fatal(state)
+	}
+}
+
+// Test liberties for a corner move
+func TestCornerLiberties(t *testing.T) {
+	state := initBoardState(makeSquareBoard(9, 9), 6)
+	ID := 0
+	err := state.moveByID(ID)
+	if err != nil { // log any error from this move
+		t.Logf("Move %v: %v\n Error: %v", 0, ID, err)
+	}
+
+	libCount := state.countLiberties(state.stringOf[ID])
+	if libCount != 2 {
+		t.Logf("Corner test: expected 2 liberties, have %v", libCount)
+		t.Fatal(state)
+	}
+}
+
+// Test liberties for a central move
+func TestCenterLiberties(t *testing.T) {
+	state := initBoardState(makeSquareBoard(9, 9), 6)
+	ID := 40
+	err := state.moveByID(ID)
+	if err != nil { // log any error from this move
+		t.Logf("Move %v: %v\n Error: %v", 0, ID, err)
+	}
+
+	libCount := state.countLiberties(state.stringOf[ID])
+	if libCount != 4 {
+		t.Logf("Corner test: expected 4 liberties, have %v", libCount)
+		t.Fatal(state)
+	}
+}
+
+func TestPlayer1(t *testing.T) {
+	state := initBoardState(makeSquareBoard(9, 9), 6)
+	if state.whiteToMove {
+		t.Fatal("whiteToMove true on initial move")
+	}
+}
