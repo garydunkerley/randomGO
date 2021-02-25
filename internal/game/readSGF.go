4dde6e66296033447cd34f781edea7ee80a297c8
garydunkerley <garydunkerley@brandeis.edu>
2021-02-25 17:37:35 -0500

    Mostly done with readSGF

--- internal/game/readSGF.go
+++ internal/game/readSGF.go
@@ -34,31 +34,6 @@ func getSquareBoardFromSGF(sgfString []string) boardTop {
 }
 
 
-// Instantiate a root.
-// Locate the first instance of a ( symbol, call it (0
-// note the location of the final move, m0,  prior to (0.
-// Move from the (0 to the next instance of a ( symbol, (1,  and repeat until you encounter a ) symbol. Take all moves encountered in this line to construct the main branch.
-
-// Now starting from the m0 encountered prior to (0,
-// parse through the file adding +1 to a counter for each
-// instance of ( and -1 for each instance of ).
-// Once the counter is zero, check for a ( symbol as this
-// will indicate that there is a new branch whose root will be
-// the move m.
-
-// How about we split the imported sgf data into two pieces: one with game metadata and one with just the moves.
-// should be possible to do this by splitting at the first instance of the substring ";B["
-
-// TODO: 	1. Split the SGF into two strings: metadata and moves
-//		2. Convert each of these into rune arrays so that we can iterate over each rune to parse branching
-
-//		3. Create either a function or a struct that outputs / stores the root and the move slice
-
-//		4. Modify the createBranch function so that it works with the object / output of the function described in 3.
-
-// Items to be converted into treeBranches need to be string, []string pairs where the first string will denote the root and the string slice will denote the move set located in this branch of the game tree.
-
-
 // separateMetadata takes the raw string for an sgf file and 
 // splits it into two strings. One containing only the metadata 
 // about the game (board size, player names, etc) and the other
@@ -78,53 +53,26 @@ func strSingletonArray(s string) []string {
 
 // getBranchGenesis takes a list of strings and a particular location and determines 
 // when the time at which the list begins to encode a new branch
-func getBranchGenesis(symbols []string, searchStart int) (int, error) {
+// and whether that new branch will be empty
+func getBranchGeneses(symbols []string, searchStart int) []int {
 	parenCounter := 0
 
 	// beginning at location searchStart, we count the number of (signed) parentheses
 	// to find where the information encoding the new branch is located.
+	var branchGeneses []int
 	for i := searchStart; i < len(symbols); i++ {
 		if symbols[i] == "(" {
 			parenCounter += 1
 		} else if symbols[i] == ")" {
 			parenCounter -= 1
 		}
-	} if parenCounter == 0 {
-		// if new branch is non-empty
-		// give the first symbol in the new branch
-
-		if symbols[i+1] == "(" {
-			return i+2
-		// if the branch is empty, return a value 
-		// that indicates such
-		} else if symbols[i+1] == ")" {
-
-			fmt.Println("Debug: New branch is empty")
-			return -1 , nil
-
-		// otherwise, there's something wrong
-		} else {
-			return -1, errors.New("Error: Unexpected symbol at beginning of new branch")
-		}
-	}
-}
-
-
-// have this output the distinct string representing the 
-// root 
-func getRootNameForBranch(symbols []string, location int) (string, error) {
-	if symbols[location] != ( {
-		return "Error", errors.New("Debug: This location is not a branching point!")
-	} else {
-		for j:= 0; j < location + 1; j++ {
-			// work backwards until you encounter a move
-			if symbols[location-j] == ";" {
-				// save the location of the move
-				rootName := isolateMoveString(location - j + 1)
-				rootName = rootName + " @(*THIS_IS_A_ROOT*)@"
+	 	if parenCounter == 0 
+			if symbols[i+1] == "("{
+				branchGeneses = append(branchGeneses, i+2)
+			} else if symbols[i+1] == ")" {
+				return branchGeneses
 			}
 		}
-	}
 }
 
 // isolateMoveString grabs all information pertaining to a move beginning at a particular location in mSymb 
@@ -145,28 +93,37 @@ func isMoveBeginning(mSymb []string, location int) bool {
 	return (mSymb[i] == ";" && (mSymb[i+1]+mSymb[i+2] == "B[" || mSymb[i+1]+mSymb[i+2] == "W[" ))   
 }
 
+// isBranchGenesis checks to see if branching is occurring at
+// specified location
+func isBranchGenesis(s []string, location int) bool {
+	return mSymb[i] == "("
+}
 
-// getMoveInput takes a string that encodes a single move, 
-// decodes the associated node color and location, 
-// and outputs the moveInput data
-getMoveInput(s string) (moveInput, error) {
+func isBranchEnd(mSymb []string, location int) bool {
+	return mSymb[i] == ")"
+}
+
+// getMove takes a string that encodes a move by 
+// decoding the associated node color and location
+func getMove(s string) (move, error) {
 	var newMoveInput moveInput 
-	
+	var newMove move
+
 	// chops off the first part of the string
 	beHeaded := strings.SplitN(s, "[", 2)
 	
-	// head will contain sonte color information
-	head = beHeaded[0]
+	// head will contain color information
+	head := beHeaded[0]
 
 	// body will be further modified to get the NodeId
-	body = beHeaded[1]
+	body := beHeaded[1]
 	 
 	if strings.Contains(head, "B") {
 			newMoveInput.playerColor = 1
 	} else if strings.Contains(head, "W") {
 		newMoveInput.playerColor = 2
 	} else {
-		return _, errors.New("Error (growBranch): 
+		return move, errors.New("Error (growBranch): 
 		String does not encode player color")
 	}
 
@@ -182,8 +139,7 @@ getMoveInput(s string) (moveInput, error) {
 
 	if len(nodeData) != 2 && nodeData != "" {
 		
-		return _, errors.New("Error (procureNodeId): 
-		String data cannot correspond to NodeId")
+		return _, errors.New("Error (procureNodeId): String data cannot correspond to NodeId")
 
 	} else if nodeData == "" {
 
@@ -195,118 +151,94 @@ getMoveInput(s string) (moveInput, error) {
 		newMoveInput.id = runeToID(nodeData[0]) + runeToID(nodeData[1])
 	}
 	
-	return newMoveInput , nil
+	newMove.moveInput = newMoveInput
+
+	return newMove, nil
 }
 
-// growBranch takes a collection of strings and a treeNode and outputs the collection of treeNodes that compose the associated branch.
-func (root *treeNode) growBranch(movesInBranch []string, branchNum int) error {
+
+
+// growBranch is a method on a treeNode that takes a slice of strings, 
+// a location in the slice, and a counter. It iteratively adds children 
+// according to the instructions starting at the specified location in 
+// mSymb. As it encounters the roots of new branches, it records them 
+// as well as the locations in mSymb corresponding to their instructions.
+// It also outputs a map that will be used to construct a global map for the tree.
+func (root *treeNode) growBranch(mSymb []string, location int, nodeCounter int) (newRoots []treeNode, newBranchGeneses []int, nodeCounter int, branchChart map[int]treeNode) {
 	
 	// a place to store the names of the treeNodes
 	branchChart := make(map[string]treeNode)
 
-	var nodeData []rune
-	var newMove move
-
-	for z := range movesInBranch {
-		// iterate over the moves and do some checks
-		
-		newMove.moveInput, _ = getMoveInput(z)
-		
-		// create a new child of the root treeNode
-		newNode := root.newChild(newMove)
-
-		// TODO: find a way to create a naming convention so that we 
-		// can identify 
-
-
-		// now set things up so that the next treeNode will have
-		// the newly created one as its parent
-		root = newNode
-			
-		
-	}
-
-}
-
-
-// buildTree takes a slice of strings (each string having length 1), records the moves comprising a given branch as well as the roots and beginnings of subsequent branches, constructing the entire tree inductively.
-func buildTree(mSymb []string, root *treeNode) error {
-	// n := getBranchCount(moveSymbols)
-
+	newRoots = []treeNode
+	var newBranchGeneses []int 
 	for z := range mSymb {
-		if len(z) != 1 {
-			return errors.New("Error: Non-singleton detected.")
+		if mSymb.isMoveBeginning(z) {
+			// make a move corresponding to the move string encoding found at location z
+			newMove := getMove(isolateMoveString(z))
+
+			// create a child of the root 
+			child := root.newChild(newMove)
+			nodeCounter += 1
+			branchChart[nodeCounter] = &child
+
+			// designate the child as the new root
+			root = child
+
+		} else if mSymb.isBranchGenesis(z) {
+			for s := range getBranchGeneses(z){
+				newRoots = append(newRoots, root)
+				newBranchGeneses = append(newBranchGeneses, s)
+			}
+		} else if mSymb.isBranchEnd(z) {
+			return newRoots, newBranchGeneses, nodeCounter, branchChart
 		}
 	}
- 
-	
-	var branchGeneses []int{0}
-	var newBranchGeneses []int
 
-	var rootLocations []int
-	var newRootLocations []int
-
-	var roots []*treeNode{root}
-	var newRoots []*treeNode
-
-	// stores the strings that encode moves in a given branch, the main branch will begin
-	// with an empty "root" node
-	var movesInBranch := []string{"root"}
-	
-	var rootMove move
-	// root should take the empty move and an empty pointer for its parent
-	root := initTreeNode(rootMove, )
-
-
-	gameTree := make([string] treeNode)
-	gameTree("root") = &root 
-
-	// iterate while there are new branches to make
-	for branchGeneses != [] {
-		newBranchGeneses = [] // set up a new save location for roots
-		for z := range branchGeneses {
-			
-			// begin constructing a branch at each root
-			for i := z; i < len(mSymb)+1; i++ {
-
-				// if the location corresponds to the beginning of a move,
-				// construct and save the string encoding that move.
-				if isMoveBeginning(mSymb, i) {
-			
-					// find the string encoding the located move and 
-					// append it to the list of moves in the branch
-
-					movesInBranch = append(movesInBranch, isolateMoveString(mSymb, i+1))
-
-				} else if mSymb[i] == "(" {
-				
-					// store the location of the root
-					newRoot := getRootForBranch(mSymb, i) 
-					newRootLocations = append(newRootLocations, newRoot) 
-
-					newBranchGenesis := getBranchGenesis(mSymb, i)
-					newBranchGeneses = append(newBranchGeneses, newBranchGenesis)
-
-					movesinBranch[len(movesinBranch)-1] = movesinBranch[len(movesinBranch-1)] + " @(*THIS_IS_A_ROOT*)@"
-				// encountering a ")" means that the branch we are on has ended 
-				} else if mSymb[i] == ")" {
-
-					constructBranch(movesInBranch)
-					// begin construction of branch 
-
-
-					// don't forget to empty the old branch!
-					// movesInBranch = []
-
-					// reset the root genesis locations to iterate over
-					// branches emerging from your newly constructed 
-					// branch
-
-					// rootGenesisLoc = newRootGenesisLoc
-				}
-			}
-		}
 }
+
+
+// buildTree takes a slice of strings (each string having length 1) and recursively constructs 
+// the gameTree by constructing a branch, for each root of a new branch, it records the root 
+// as well as the location in mSymb where the new branch is encoded. At each node initialization, 
+// a map associates an integer with the new node.  
+// The code iterates until there are no more branches to grow.
+func buildTree(mSymb []string) map[int]treeNode, error {
+
+	var rootMove move
+	root := initTreeNode(rootMove, )
+	roots := []*treeNode{root}
+
+	branchGeneses := []int{0}
+	branchesToGrow := true
+
+	nodeCounter := 0
+	gameTree := make(map[int]treeNode)
+
+	// while there are still branches to grow, construct branches
+	for branchesToGrow {
+		if len(roots) != len(branchGeneses) {
+			return _, errors.New("Error (buildTree): len(roots) =/= len(branchGeneses) ")
+		}
+		newRoots, newGeneses, nodeCounter, chart = roots[0].growBranch(mSymb, branchGeneses[0], nodeCounter)
+		
+		for z := range chart {
+			gameTree[z] = chart[z]
+		}
+
+
+		roots = append(roots[1:], newRoots)
+		branchGeneses = append(branchGeneses[1:], newBranchGeneses)
+		
+		// if there are no new branches to grow, end the loop
+		if len(roots) == 0 {
+			branchesToGrow = false
+		}
+	}
+
+	return gameTree, nil
+
+}
+
 
 // initTreeNode specifies a node in the game tree by a move, treeNode pointer pair.
 func initTreeNode(value move, parent *treeNode) *treeNode {
