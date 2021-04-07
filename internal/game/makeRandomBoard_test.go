1b1f863539b5d079ed0c77b4f79e8502d591a02c
Justin Rogers <justin_rogers@math.ucsb.edu>
2021-04-07 13:40:33 -0700

    Add benchmarks for hex creation

---
+++ internal/game/makeRandomBoard_test.go
@@ -0,0 +1,22 @@
+package game
+
+import "testing"
+
+func BenchmarkMakeRandomBoard(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		makeRandomBoard(100)
+	}
+}
+
+func BenchmarkGetHexagonalLattice(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		getHexagonalLattice(100)
+	}
+}
+
+func BenchmarkGetLatticeAndSparsify(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		edges, coords := getHexagonalLattice(100)
+		_ = sparsifyEdges(100, edges, coords)
+	}
+}
