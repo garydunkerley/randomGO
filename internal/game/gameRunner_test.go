2ea1cc81f02156a8f3bfbe3e43e0170fd7869eab
gary <garydunkerley@brandeis.edu>
2021-04-08 13:16:03 -0500

    Restored gameRunner_test.go

---
+++ internal/game/gameRunner_test.go
@@ -0,0 +1,120 @@
+package game
+
+import "testing"
+
+// Test a behavior where nonlocal corner capture occurs
+func TestMove(t *testing.T) {
+	state := initBoardState(makeSquareBoard(9, 9), 6)
+	// Play a sequence of moves that gives bad behavior
+	moveSequence := []int{52, 0, 53}
+	for i, ID := range moveSequence {
+		err := state.moveByID(ID)
+		if err != nil { // log any error from this move
+			t.Logf("Move %v: %v\n Error: %v", i, ID, err)
+		}
+	}
+	// Fail if last move made a capture
+	lastMove := state.moves[len(state.moves)-1]
+	if lastMove.capturesMade != 0 {
+		t.Logf("Captures last move: %v\n", lastMove.capturesMade)
+		t.Fatal(state) // log end state
+	}
+}
+
+// Test liberties for an edge move
+func TestEdgeLiberties(t *testing.T) {
+	state := initBoardState(makeSquareBoard(9, 9), 6)
+	ID := 1
+	err := state.moveByID(ID)
+	if err != nil { // log any error from this move
+		t.Logf("Move %v: %v\n Error: %v", 0, ID, err)
+	}
+
+	libCount := state.countLiberties(state.stringOf[ID])
+	if libCount != 3 {
+		t.Logf("Edge test: expected 3 liberties, have %v", libCount)
+		t.Logf("Liberty IDs: %v", getLiberties(state.nodes[ID]))
+		t.Logf("Edge IDs (boardTop): %v", state.edges[ID])
+		t.Logf("Neighbor count (node-level): %v",
+			len(state.nodes[ID].neighbors))
+		for _, y := range state.nodes[ID].neighbors {
+			t.Logf("Neighbor ID %v: color %v", y.id, y.color)
+		}
+		t.Fatal(state)
+	}
+}
+
+// Test liberties for a corner move
+func TestCornerLiberties(t *testing.T) {
+	state := initBoardState(makeSquareBoard(9, 9), 6)
+	ID := 0
+	err := state.moveByID(ID)
+	if err != nil { // log any error from this move
+		t.Logf("Move %v: %v\n Error: %v", 0, ID, err)
+	}
+
+	libCount := state.countLiberties(state.stringOf[ID])
+	if libCount != 2 {
+		t.Logf("Corner test: expected 2 liberties, have %v", libCount)
+		t.Logf("Liberty IDs: %v", getLiberties(state.nodes[ID]))
+		t.Logf("Edge IDs (boardTop): %v", state.edges[ID])
+		t.Logf("Neighbor count (node-level): %v",
+			len(state.nodes[ID].neighbors))
+		for _, y := range state.nodes[ID].neighbors {
+			t.Logf("Neighbor ID %v: color %v", y.id, y.color)
+		}
+		t.Fatal(state)
+	}
+}
+
+// Test liberties for a central move
+func TestCenterLiberties(t *testing.T) {
+	state := initBoardState(makeSquareBoard(9, 9), 6)
+	ID := 40
+	err := state.moveByID(ID)
+	if err != nil { // log any error from this move
+		t.Logf("Move %v: %v\n Error: %v", 0, ID, err)
+	}
+
+	libCount := state.countLiberties(state.stringOf[ID])
+	if libCount != 4 {
+		t.Logf("Corner test: expected 4 liberties, have %v", libCount)
+		t.Logf("Liberty IDs: %v", getLiberties(state.nodes[ID]))
+		t.Logf("Edge IDs (boardTop): %v", state.edges[ID])
+		t.Logf("Neighbor count (node-level): %v",
+			len(state.nodes[ID].neighbors))
+		for _, y := range state.nodes[ID].neighbors {
+			t.Logf("Neighbor ID %v: color %v", y.id, y.color)
+		}
+		t.Fatal(state)
+	}
+}
+
+func TestPlayer1(t *testing.T) {
+	state := initBoardState(makeSquareBoard(9, 9), 6)
+	if state.whiteToMove {
+		t.Fatal("whiteToMove true on initial move")
+	}
+}
+
+func TestSwitchOnPass(t *testing.T) {
+	state := initBoardState(makeSquareBoard(9, 9), 6)
+	previousPlayer := state.whiteToMove
+	state.moveByID(-1)
+	if state.whiteToMove == previousPlayer {
+		t.Logf("Player not switching off on pass.")
+		t.Fatal()
+	}
+}
+
+/*
+func ColorSwitchOnPass(t *testing.T) {
+	state := initBoardState(makeSquareBoard(9, 9), 6)
+	desiredColor := 2
+	state.moveByID(-1)
+	state.MoveByID(0)
+	if state.goGraph[0].color != 2 {
+		t.Logf("Stone color does not match player color.")
+	}
+}
+*/
